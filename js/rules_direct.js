04b0b5c69adbbf751edc2b6e75d146113cd3c67e
Watch Your Back <3292722614@qq.com>
2026-03-21 17:30:02 +0800

    Update rules_direct.js

--- js/rules_direct.js
+++ js/rules_direct.js
@@ -4,14 +4,18 @@ window.RULES_DIRECT = {
         "url": "https://blog.142588.xyz",
         "expired_at": ""
     },
+
+    
+    // === 主页 ===
     "/home": {
-        "url": "https://home.142588.xyz",
+        "title": "选择主页",
+        "urls": [
+            { "name": "主页", "url": "https://home.142588.xyz" },
+            { "name": "主页（备）", "url": "https://home.142587.de5.net" }
+        ],
         "expired_at": ""
     },
-    "/home2": {
-        "url": "https://page.142588.xyz",
-        "expired_at": ""
-    },
+
 
     // === AI 服务 ===
     "/ai": {
@@ -26,126 +30,80 @@ window.RULES_DIRECT = {
         ],
         "expired_at": ""
     },
-    "/chat": {
-        "url": "https://chat.142588.xyz",
-        "expired_at": ""
-    },
-    "/chatai": {
-        "url": "https://ai.142588.xyz",
-        "expired_at": ""
-    },
-    "/videoai": {
-        "url": "https://videoai.142588.xyz",
-        "expired_at": ""
-    },
-    "/imgai": {
-        "url": "https://imageai.142588.xyz",
-        "expired_at": ""
-    },
-    "/imgai2": {
-        "url": "https://text2img.fafu.indevs.in",
-        "expired_at": ""
-    },
-    "/tts": {
-        "url": "https://tts.142587.de5.net",
-        "expired_at": ""
-    },
+
+
 
     // === 开发工具与代理 ===
     "/p": {
         "title": "选择代理/订阅服务",
         "urls": [
-            { "name": "网页代理", "url": "https://edit.142588.xyz" },
-            { "name": "Clash 订阅", "url": "https://clash.142588.xyz" },
-            { "name": "通用订阅转换", "url": "https://tunsub.142588.xyz" },
-            { "name": "CF 节点", "url": "https://cfnode.142588.xyz" },
-            { "name": "备用订阅 1", "url": "https://sub.142587.de5.net" },
-            { "name": "备用订阅 2", "url": "https://sub.cro.cc.cd" }
+            { "name": "网页代理", "url": "https://client.142588.xyz" },
+            { "name": "备用订阅 1", "url": "https://node.142588.xyz" },
+            { "name": "备用订阅 2", "url": "https://tunsub.142588.xyz" },
+            { "name": "备用订阅 3", "url": "https://cfnode.142588.xyz" },
+            { "name": "备用订阅 4", "url": "https://sub.142587.de5.net" },
+            { "name": "通用订阅转换", "url": "https://sub.cro.cc.cd" }
         ],
         "expired_at": ""
     },
-    "/github": {
-        "url": "https://gh.142588.xyz",
-        "expired_at": ""
-    },
-    "/github2": {
-        "url": "https://pro.142588.xyz",
-        "expired_at": ""
-    },
-    "/proxy": {
-        "url": "https://edit.142588.xyz",
-        "expired_at": ""
-    },
-    "/proxy2": {
-        "url": "https://clash.142588.xyz",
-        "expired_at": ""
-    },
-    "/proxy3": {
-        "url": "https://tunsub.142588.xyz",
-        "expired_at": ""
-    },
-    "/proxy4": {
-        "url": "https://cfnode.142588.xyz",
-        "expired_at": ""
-    },
-    "/sub": {
-        "url": "https://sub.142587.de5.net",
-        "expired_at": ""
-    },
-    "/sub2": {
-        "url": "https://sub.cro.cc.cd",
-        "expired_at": ""
-    },
-    "/wxnode": {
-        "url": "https://wxpro.cro.cc.cd",
+
+    // === github加速 ===
+    "/gh": {
+        "title": "选择加速源",
+        "urls": [
+            { "name": "加速源", "url": "https://gh.142588.xyz" },
+            { "name": "加速源（备）", "url": "https://pro.142588.xyz" }
+        ],
         "expired_at": ""
     },
 
+
+
+    // === 短链接导航 ===
+    "/short": {
+             "title": "短链接导航",
+             "urls": [
+              { "name": "短链接 1", "url": "https://link.142588.xyz" },
+              { "name": "短链接 2", "url": "https://short.fung.us.ci" },
+              { "name": "短链接 3", "url": "https://w.142588.xyz/_url" }
+             ],
+             "expired_at": ""
+        },
+
+        
+
     // === 实用工具 ===
     "/tool": {
-        "url": "https://tool.cro.cc.cd",
+             "title": "工具集合",
+             "urls": [
+              { "name": "工具箱", "url": "https://tool.142588.xyz" },
+              { "name": "密码管理", "url": "https://pass.142588.xyz" },
+              { "name": "水印", "url": "https://watermark.142588.xyz" },
+              { "name": "图床", "url": "https://imgbed.142588.xyz" },
+              { "name": "云盘", "url": "https:/drive.142588.xyz" },
+              { "name": "虚拟地址", "url": "https://address.142588.xyz" }
+             ],
+             "expired_at": ""
+        },
+
+
+
+    // === 艺术设计 ===
+    "/art": {
+        "title": "选择浏览",
+        "urls": [
+            { "name": "主页", "url": "https://cover.142587.de5.net" },
+            { "name": "主页（备）", "url": "https://cover.142588.xyz" },
+            { "name": "主页（备）", "url": "https://wallpaper.142588.xyz }
+            
+        ],
         "expired_at": ""
     },
-    "/ip": {
-        "url": "https://ip.fung.us.ci",
-        "expired_at": ""
-    },
-    "/password": {
-        "url": "https://pass.142588.xyz",
-        "expired_at": ""
-    },
-    "/watermark": {
-        "url": "https://watermark.142588.xyz",
-        "expired_at": ""
-    },
-    "/wallpaper": {
-        "url": "https://wallpaper.142588.xyz",
-        "expired_at": ""
-    },
-    "/imgbed": {
-        "url": "https://imgbed.142588.xyz",
-        "expired_at": ""
-    },
-    "/drive": {
-        "url": "https://drive.142588.xyz",
-        "expired_at": ""
-    },
-    "/cover": {
-        "url": "https://cover.142587.de5.net",
-        "expired_at": ""
-    },
-    "/cover2": {
-        "url": "https://cover.142588.xyz",
-        "expired_at": ""
-    },
-    "/adderss": {
-        "url": "https://address.142588.xyz",
-        "expired_at": ""
-    },
-    "/piyao": {
-        "url": "https://wxpiyao.142588.xyz",
-        "expired_at": ""
-    },
+
+
+    
+
+
 
     // === 社交与通信 ===
     "/mail": {
@@ -164,16 +122,8 @@ window.RULES_DIRECT = {
         "url": "https://wxdown.fafu.indevs.in",
         "expired_at": ""
     },
-    "/paste": {
-        "url": "https://paste.142588.xyz",
-        "expired_at": ""
-    },
-    "/paste2": {
-        "url": "https://notepad.142588.xyz",
-        "expired_at": ""
-    },
-    "/paste3": {
-        "url": "https://message.ucce.eu.cc",
+    "/piyao": {
+        "url": "https://wxpiyao.142588.xyz",
         "expired_at": ""
     },
     "/discuss": {
@@ -181,19 +131,43 @@ window.RULES_DIRECT = {
         "expired_at": ""
     },
 
-    // === 导航、新闻与分析 ===
-    "/nav": {
-        "url": "https://nav.142588.xyz",
+    
+
+    // === 文字传输 ===
+    "/home": {
+        "title": "选择网址",
+        "urls": [
+            { "name": "主页", "url": "https://paste.142588.xyz" },
+            { "name": "主页（备）", "url": "https://paste.142587.de5.net" }
+        ],
         "expired_at": ""
     },
-    "/nav2": {
-        "url": "https://nav3.142588.xyz",
+
+
+    // === 导航 ===
+    "/home": {
+        "title": "选择导航页",
+        "urls": [
+            { "name": "主页", "url": "https://nav.142588.xyz" },
+            { "name": "主页（备）", "url": "https://nav.cro.cc.cd" }
+        ],
         "expired_at": ""
     },
-    "/nav3": {
-        "url": "https://nav2.142588.xyz",
+
+
+
+
+    // === 新闻资讯 ===
+    "/home": {
+        "title": "点击跳转",
+        "urls": [
+            { "name": "主页", "url": "https://home.142588.xyz" },
+            { "name": "主页（备）", "url": "https://home.142587.de5.net" }
+        ],
         "expired_at": ""
     },
+
+
     "/search": {
         "url": "https://search.142588.xyz",
         "expired_at": ""
@@ -215,20 +189,9 @@ window.RULES_DIRECT = {
         "expired_at": ""
     },
 
-    // === 短链服务 ===
-    "/short": {
-        "url": "https://link.142588.xyz",
-        "expired_at": ""
-    },
-    "/short2": {
-        "url": "https://short.fung.us.ci",
-        "expired_at": ""
-    },
-    "/short3": {
-        "url": "https://w.142588.xyz/_url",
-        "expired_at": ""
-    },
 
+
+    
     // === 其他 ===
     "/pay": {
         "url": "https://pay.142588.xyz",
